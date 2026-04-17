cad803cd2539a3626fc96ef1ac466e030a50d826
Chris J Edwards <christopher.j.edwards@hotmail.com>
2026-04-17 15:50:11 +0200

    Add greeting and fun text in TestaGitHub.py
    Added a greeting and fun text from Chris.

--- TestaGitHub.py
+++ TestaGitHub.py
@@ -4,3 +4,7 @@ print("Nu har jag ändrat i min kod och pushat upp det till GitHub igen")
 print("Nu har jag ändrat i min kod och pushat upp det till GitHub igen och igen")
 FavoritFärg = ["Röd", "Blå", "Grön", "Gul", "Lila"]
 print("Mina favorit färger är: ", FavoritFärg)
+# Chris lägger till lite rolig text
+print("Nu styr Chris lite rolig text")
+name = input("Vad heter du?")
+print("Hej", name, "hoppas du får en trevlig kväll med barnbarnet!")
