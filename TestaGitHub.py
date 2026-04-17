e66b621fe2408d94c28e21866d7d8e6b43b87df5
GabyCM78 <gabrielacaligari@hotmail.com>
2026-04-17 14:05:02 +0200

    Sparar första versionen

---
+++ TestaGitHub.py
@@ -0,0 +1,6 @@
+print("Hejsan hallå GitHub")
+print("Nu har jag ändrat i min kod och pushat upp det till GitHub")
+print("Nu har jag ändrat i min kod och pushat upp det till GitHub igen")
+print("Nu har jag ändrat i min kod och pushat upp det till GitHub igen och igen")
+FavoritFärg = ["Röd", "Blå", "Grön", "Gul", "Lila"]
+print("Mina favorit färger är: ", FavoritFärg)
